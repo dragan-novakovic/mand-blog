e86bc38e030aab47b5e27054b5778609366d86cb
dragan1810 <d.novakovic1810@yahoo.com>
2019-12-29 22:59:32 +0100

    testing svelte

---
+++ src/App.svelte
@@ -0,0 +1,70 @@
+<script>
+  import Icon from "svelte-awesome";
+  import { userCircleO, globe, eye } from "svelte-awesome/icons";
+  import Header from "./components/Header.svelte";
+  import Carousel from "./components/Carousel.svelte";
+  import Card from "./components/Card.svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    background-color: whitesmoke;
+  }
+  article {
+    background-color: cadetblue;
+    max-height: 500px;
+    margin-top: 16px;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+  }
+
+  .tabs {
+    margin: 50px 0;
+  }
+
+  ul li {
+    margin-left: 10px;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 16px;
+  }
+
+  .cards-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
+
+<main>
+  <Header />
+  <article>
+    <Carousel />
+  </article>
+  <div class="tabs">
+    <ul>
+      <li style="color: green;">
+        <Icon data={userCircleO} />
+        <span>People</span>
+      </li>
+      <li style="color: purple;">
+        <Icon data={globe} />
+        <span>Places</span>
+      </li>
+      <li style="color: blue;">
+        <Icon data={eye} />
+        <span>Experiences</span>
+      </li>
+    </ul>
+  </div>
+  <div class="cards-container">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</main>
---
+++ src/components/Card.svelte
@@ -0,0 +1,65 @@
+<script>
+  import Icon from "svelte-awesome";
+  import { eye } from "svelte-awesome/icons";
+</script>
+
+<style>
+  article {
+    background-color: white;
+    min-width: 400px;
+    max-width: 40vw;
+    margin: 20px auto;
+    box-shadow: 1px 1px 5px grey;
+  }
+  .card-bg {
+    min-width: 400px;
+    max-width: 40vw;
+  }
+  .author {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .author h4 {
+    margin-left: 1em;
+  }
+  .avatar {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    margin: 10px 0 0 10px;
+  }
+
+  h2 {
+    text-align: left;
+    margin-left: 10px;
+  }
+  .circle {
+    position: relative;
+    bottom: 20px;
+    left: 90%;
+    width: 26px;
+    height: 26px;
+    border-radius: 50%;
+    background-color: #379;
+  }
+</style>
+
+<article>
+  <div class="image-container">
+    <img class="card-bg" src="./images/railway.jpg" alt="placeholder" />
+    <div class="circle">
+      <Icon
+        style="color: white; padding-top:2px; padding-left:1px;"
+        data={eye}
+        scale="1.2" />
+    </div>
+  </div>
+  <div class="author">
+    <img class="avatar" src="./images/railway.jpg" alt="placeholder" />
+    <h4>Don Joe</h4>
+  </div>
+  <h2>
+    Why winter is best time to tacle west sibira trainstation express train
+  </h2>
+</article>
